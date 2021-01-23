f5f587ecf19d65a42433a15132b10573dfe49009
Taichi Ishitani <taichi730@gmail.com>
2021-01-23 22:48:31 +0900

    implement properties of counter type

--- lib/rggen/my_plugin/bit_field/type/counter.rb
+++ lib/rggen/my_plugin/bit_field/type/counter.rb
@@ -2,6 +2,10 @@
 
 RgGen.define_list_item_feature(:bit_field, :type, :counter) do
   register_map do
+    read_write
+    volatile
+    initial_value require: true
+    reference use: true, width: 1
   end
 
   sv_rtl do
